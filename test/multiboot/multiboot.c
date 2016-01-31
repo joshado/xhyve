4a80bb29e9c354d5df2905241d95043a1790fa99
Thomas Haggett <thomas@haggett.org>
2016-01-31 20:20:54 +0000

    Such hacks. Writing out to the com-port, wired up to stdio in qemu. Lets attack xhyve!

--- test/multiboot/multiboot.c
+++ test/multiboot/multiboot.c
@@ -1,4 +1,7 @@
 
+typedef unsigned long long int uint64_t;
+typedef unsigned short int uint16_t;
+typedef unsigned char uint8_t;
 
 void multiboot_entry(void);
 
@@ -22,10 +25,22 @@ void main(void) {
 }
 
 
+static inline void outb(uint16_t port, uint8_t val)
+{
+    asm volatile ( "outb %0, %1" : : "a"(val), "Nd"(port) );
+}
+static inline uint8_t inb(uint16_t port)
+{
+    uint8_t ret;
+    asm volatile ( "inb %[port], %[ret]"
+                   : [ret] "=a"(ret)   // using symbolic operand names as an example, mainly because they're not used in order
+                   : [port] "Nd"(port) );
+    return ret;
+}
+
 
 static volatile unsigned char *video;
 
-typedef unsigned long long int uint64_t;
 
 static inline uint64_t rdtsc()
 {
@@ -43,9 +58,30 @@ void sleep(int count) {
 #define COLUMNS                 80
 #define LINES                   24
 
+#define COM1  0x3f8
+
+void com_write(char v) {
+  // wait for the transmit buffer to empty
+  while(inb(COM1 + 5) & 0x20 == 0 ) { }
+
+    // write the value!
+    outb(COM1, v);
+}
+
+void com_write_string(char* string) {
+
+  while(*string != 0x0) {
+    com_write(*string++);
+  }
+}
+
+
 void multiboot_entry() {
   int i = 0;
   char v = 0;
+
+  com_write_string("\n");
+  com_write_string("Booting tinykernel!\n");
 
   while(1) {
     video = (unsigned char *) VIDEO;
@@ -55,7 +91,7 @@ void multiboot_entry() {
 
     }
     v++;
-
+    com_write_string("Loop!\n");
 
     sleep(100000000);
   }
