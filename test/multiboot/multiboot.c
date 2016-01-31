3e2cf343e7aa79746df93ca048479e018ae9c1da
Thomas Haggett <thomas@haggett.org>
2016-01-31 18:15:57 +0000

    Got a multi boot image that writes crap out to the vga device (to make sure qemu will boot it)

---
+++ test/multiboot/multiboot.c
@@ -0,0 +1,45 @@
+
+
+void multiboot_entry(void);
+
+void main(void) {
+  asm (
+    "start:"
+    ".align 8\n\t"
+    "multiboot_header:"
+    ".long 0x1BADB002\n\t"
+    ".long 0x10003\n\t"
+    ".long 0xe4514ffb\n\t"
+    ".long   multiboot_header\n\t"  // header_addr
+    ".long   start\n\t"            // load_addr
+    ".long   0\n\t"                 // load_end_addr
+    ".long   0\n\t"                 // bss_end_addr
+    ".long   _multiboot_entry\n\t"   // entry_addr
+  );
+
+  multiboot_entry();
+  while(1) { }
+}
+
+#define VIDEO                   0xB8000
+#define COLUMNS                 80
+#define LINES                   24
+#define ATTRIBUTE               7
+
+static volatile unsigned char *video;
+
+void multiboot_entry() {
+  int i = 0;
+  char v = 0;
+  while(1) {
+    video = (unsigned char *) VIDEO;
+
+    for (i = 0; i < COLUMNS * LINES * 2; i++) {
+      *(video + i) = v++;
+
+    }
+    v++;
+
+
+  }
+}
