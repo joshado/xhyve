e9abb6299be7d0cd9f4262022d81ea45ab2d91d7
Thomas Haggett <thomas@haggett.org>
2016-02-01 21:12:34 +0000

    Clean up the tiny test kernel.

--- test/multiboot/multiboot.c
+++ test/multiboot/multiboot.c
@@ -1,29 +1,23 @@
 
-typedef unsigned long long int uint64_t;
-typedef unsigned short int uint16_t;
-typedef unsigned char uint8_t;
+void main(void);
 
-void multiboot_entry(void);
+asm (
+  "start:\n\t"
+  "multiboot_header:\n\t"
+  ".balign 4\n\t"
+  ".long 0x1BADB002\n\t"
+  ".long 0x10003\n\t"
+  ".long 0xe4514ffb\n\t"
+  ".long   multiboot_header\n\t"  // header_addr
+  ".long   start\n\t"             // load_addr
+  ".long   0\n\t"                 // load_end_addr
+  ".long   0\n\t"                 // bss_end_addr
+  ".long   _main\n\t"   // entry_addr
+  ".space 65536\n\t"
+);
 
-void main(void) {
-  asm (
-    "start:"
-    ".align 4\n\t"
-    "multiboot_header:"
-    ".long 0x1BADB002\n\t"
-    ".long 0x10003\n\t"
-    ".long 0xe4514ffb\n\t"
-    ".long   multiboot_header\n\t"  // header_addr
-    ".long   start\n\t"            // load_addr
-    ".long   0\n\t"                 // load_end_addr
-    ".long   0\n\t"                 // bss_end_addr
-    ".long   _multiboot_entry\n\t"   // entry_addr
-  );
 
-  multiboot_entry();
-  while(1) { }
-}
-
+#include <stdint.h>
 
 static inline void outb(uint16_t port, uint8_t val)
 {
@@ -32,14 +26,11 @@ static inline void outb(uint16_t port, uint8_t val)
 static inline uint8_t inb(uint16_t port)
 {
     uint8_t ret;
-    asm volatile ( "inb %[port], %[ret]"
-                   : [ret] "=a"(ret)   // using symbolic operand names as an example, mainly because they're not used in order
-                   : [port] "Nd"(port) );
+    asm volatile ( "inb %[port], %[ret]" : [ret] "=a"(ret) : [port] "Nd"(port) );
     return ret;
 }
 
-
-static volatile unsigned char *video;
+static volatile uint8_t *video;
 
 
 static inline uint64_t rdtsc()
@@ -49,7 +40,8 @@ static inline uint64_t rdtsc()
     return ret;
 }
 
-void sleep(int count) {
+void sleep(int count) 
+{
   uint64_t entry = rdtsc();
   while(rdtsc() < entry + count) { }
 }
@@ -60,23 +52,24 @@ void sleep(int count) {
 
 #define COM1  0x3f8
 
-void com_write(char v) {
+void com_write(char v) 
+{
   // wait for the transmit buffer to empty
-  while(inb(COM1 + 5) & 0x20 == 0 ) { }
+  while((inb(COM1 + 5) & 0x20) == 0 ) { }
 
     // write the value!
     outb(COM1, v);
 }
 
-void com_write_string(char* string) {
-
-  while(*string != 0x0) {
+void com_write_string(char* string) 
+{
+  while(*string != 0x0)
     com_write(*string++);
-  }
 }
 
 
-void multiboot_entry() {
+void main() 
+{
   int i = 0;
   char v = 0;
 
